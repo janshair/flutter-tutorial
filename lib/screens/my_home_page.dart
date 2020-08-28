1345e8b1477d17feca0191ad682da6158927d9ed
Jan Shair <janshair@tapdevs.com>
2020-08-28 21:36:20 +0100

    first commit

---
+++ lib/screens/my_home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        leading: Icon(Icons.menu),
+        title: Text('My Home Page'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right:10.0),
+            child: Icon(Icons.settings),
+          )
+        ],
+      ),
+    );
+  }
+}
